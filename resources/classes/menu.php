af9fc9d5d343d8e32c7ec327b1724befe5fda98a
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-01-23 15:06:42 +0330

    fix bug

--- resources/classes/menu.php
+++ resources/classes/menu.php
@@ -1173,7 +1173,7 @@ if (!class_exists('menu')) {
 					}
 				//logout icon
 					if (!empty($_SESSION['username']) && $_SESSION['theme']['logout_icon_visible']['text'] == "true") {
-						$html .= "<a id='header_logout_icon' href='#' class='header-avatar' title=\"".$this->text['theme-label-logout']."\" onclick=\"modal_open('modal-logout','btn_logout');\">خروج<span class='fas fa-sign-out-alt'></span></a>";
+						$html .= "<a id='header_logout_icon' href='#' class='header-avatar' title=\"".$this->text['theme-label-logout']."\" onclick=\"modal_open('modal-logout','btn_logout');\"><span class='fas fa-sign-out-alt'></span>خروج</a>";
 					}
 				$html .= "</div>";
 			$html .= "	</div>\n";

1818b82018bd837f678739c97ab7d1fd2adf5dc4
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-01-23 14:05:13 +0330

    fix bug

--- themes/default/template.php
+++ themes/default/template.php
@@ -7,7 +7,7 @@
 		<!DOCTYPE html>
 	{/if}
 
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='fa' lang='fa'>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
 <head>
 <meta charset='utf-8'>
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
@@ -21,8 +21,6 @@
 	<link rel='stylesheet' type='text/css' href='{$project_path}/resources/bootstrap/css/bootstrap-colorpicker.min.css.php'>
 	<link rel='stylesheet' type='text/css' href='{$project_path}/resources/fontawesome/css/all.min.css.php'>
 	<link rel='stylesheet' type='text/css' href='{$project_path}/themes/default/css.php'>
-		<link rel='stylesheet' type='text/css' href='{$project_path}/themes/default/assets/font/fontiran.css'>
-	<link rel='stylesheet' type='text/css' href='{$project_path}/themes/default/assets/css/rtl.css'>
 {*//link to custom css file *}
 	{if !empty($settings.theme.custom_css)}
 		<link rel='stylesheet' type='text/css' href='{$settings.theme.custom_css}'>
@@ -1043,7 +1041,7 @@
 
 				//add new options from the json results
 				for (var i=0; i < obj.length; i++) {
-					
+
 					//get the variables
 					domain_uuid = obj[i].domain_uuid;
 					domain_name = obj[i].domain_name;
@@ -1068,7 +1066,7 @@
 						div.style.background = '{$domain_selector_background_color_2}';
 					}
 
-					//set the active domain style 
+					//set the active domain style
 					if ('{$domain_uuid}' == obj[i].domain_uuid) {
 						div.style.background = '{$domain_active_background_color}';
 						div.style.fontWeight = 'bold';
@@ -1145,16 +1143,12 @@
 
 	{*//login page *}
 		{if !empty($login_page)}
-		    <div class="container">
-		        <div class="col-lg-6 col-12">
-                    <div id='codarx_login'>
-                        <a href='{$project_path}/'><img id='login_logo' style='width: {$login_logo_width}; height: {$login_logo_height};' src='{$login_logo_source}'></a><br />
-                    </div>
-
-                    <div id='footer_login'>
-                        <span class='footer'>{$settings.theme.footer}</span>
-                    </div>
-                </div>
+			<div id='default_login'>
+				<a href='{$project_path}/'><img id='login_logo' style='width: {$login_logo_width}; height: {$login_logo_height};' src='{$login_logo_source}'></a><br />
+				{$document_body}
+			</div>
+			<div id='footer_login'>
+				<span class='footer'>{$settings.theme.footer}</span>
 			</div>
 
 	{*//other pages *}

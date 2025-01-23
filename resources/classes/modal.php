a7c982ca1ba4eecccd2317d01e96eec0b898c3f4
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-01-23 16:52:42 +0330

    fix bug

--- resources/classes/modal.php
+++ resources/classes/modal.php
@@ -56,7 +56,7 @@ if (!class_exists('modal')) {
 							$array['title'] = !empty($array['title']) ? $array['title'] : $text['modal_title-confirmation'];
 					}
 				//prefix cancel button to action
-					$array['actions'] = button::create(['type'=>'button','label'=>$text['button-cancel'],'icon'=>$_SESSION['theme']['button_icon_cancel'],'collapse'=>'never','onclick'=>'modal_close(); '.($array['onclose'] ?? '')]).$array['actions'];
+					$array['actions'] = button::create(['type'=>'button','label'=>'انصراف','icon'=>$_SESSION['theme']['button_icon_cancel'],'collapse'=>'never','onclick'=>'modal_close(); '.($array['onclose'] ?? '')]).$array['actions'];
 			}
 			$modal .= !empty($array['title']) ? "		<span class='modal-title'>".$array['title']."</span>\n" : null;
 			$modal .= !empty($array['message']) ? "		<span class='modal-message'>".$array['message']."</span>\n" : null;

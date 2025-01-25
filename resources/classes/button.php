aa43db7e18b9b4cb7c38ad8924a77e49f59ecedd
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-01-25 18:09:09 +0330

    fix bug

--- resources/classes/button.php
+++ resources/classes/button.php
@@ -86,7 +86,7 @@ if (!class_exists('button')) {
 					)) {
 					$icon_class = is_array($array['icon']) ? $array['icon']['text'] : 'fas fa-' . $array['icon'];
 					if (is_array($array['icon'])) {
-						$button .= "<img src='../../themes/default/images/icon/" . $array['icon']['text'] . "'.svg />";
+						$button .= "<img src='../../themes/default/images/icon/" . $array['icon']['text'] . ".svg' />";
 
 					}
 //					$button .= "<span class='".$icon_class." fa-fw'></span>";

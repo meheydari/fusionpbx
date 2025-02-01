288a1f915eab0c3366e0d1b75d76579fb97b9128
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-02-01 13:57:51 +0330

    translate

--- resources/classes/button.php
+++ resources/classes/button.php
@@ -110,12 +110,15 @@ if (!class_exists('button')) {
 						}
 					}
 					$pad_class = !empty($array['icon']) ? 'pad' : null;
-					if ($array['codarx'] == 'boolean_icon') {
+					if ($array['codarx'] == 'boolean_icon' || true) {
 						if ($array['label'] == 'True') {
 							$button .= "<img style='padding: 10px' src='../../themes/default/images/icon/done.svg'/>";
 						}
 						if ($array['label'] == 'False') {
 							$button .= "<img style='padding: 10px' src='../../themes/default/images/icon/fail.svg'/>";
+						}else{
+							$button .= "<span class='button-label " . ($collapse_class ?? '') . " " . $pad_class . "'>" . $array['label'] . "</span>";
+
 						}
 					} else {
 						$button .= "<span class='button-label " . ($collapse_class ?? '') . " " . $pad_class . "'>" . $array['label'] . "</span>";

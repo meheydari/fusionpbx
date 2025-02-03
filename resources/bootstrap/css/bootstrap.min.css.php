f33faa2f4033c407e20cc2971c2b55c8e33ddbae
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-02-03 09:03:39 +0330

    translate

--- resources/bootstrap/css/bootstrap.min.css.php
+++ resources/bootstrap/css/bootstrap.min.css.php
@@ -7,7 +7,7 @@ header('Expires: '.gmdate('D, d M Y H:i:s',time()+3600).' GMT');
 
 echo $_SESSION['app']['languages'];
 echo $_SESSION['domain']['language']['code'];
-var_dump($_SESSION['domain']);
+var_dump($_SESSION);
 include_once 'bootstrap.min.css';
 
 ?>

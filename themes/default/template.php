c1706fa6b9c97b715097f27322e33056a9335b00
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-02-03 08:53:18 +0330

    translate

--- themes/default/template.php
+++ themes/default/template.php
@@ -9,7 +9,7 @@ $language_code = $_SESSION['domain']['language']['code'];
 		<!DOCTYPE html>
 	{/if}
 
-<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='{$settings.domain.language}' lang='{$_SESSION.domain.language.code}'>
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='{$settings.domain.language.code}' lang='{$_SESSION.domain.language.code}'>
 <head>
 <meta charset='utf-8'>
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>

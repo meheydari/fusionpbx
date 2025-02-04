79db97d000144675f230db43dd3099435d73a899
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-02-04 12:12:04 +0330

    translate

--- resources/classes/menu.php
+++ resources/classes/menu.php
@@ -1135,6 +1135,7 @@ if (!class_exists('menu')) {
 						//sub menu items
 							if (is_array($menu_item_main['menu_items']) && sizeof($menu_item_main['menu_items']) != 0) {
 								$html .= "	<div id='sub_".$menu_item_main['menu_item_uuid']."' class='menu_side_sub' style='display: none;'>\n";
+								var_dump($menu_item_main['menu_items']);
 								foreach ($menu_item_main['menu_items'] as $menu_index_sub => $menu_item_sub) {
 									$html .= "		<a class='menu_side_item_sub' ".($menu_item_sub['menu_item_category'] == 'external' ? "target='_blank'" : null)." title='".$menu_item_sub['menu_language_title']."' href='".$menu_item_sub['menu_item_link']."'>";
 									$html .= 			"<span class='menu_side_item_title' ".($_SESSION['theme']['menu_side_state']['text'] != 'expanded' ? "style='display: none;'" : null).">".$menu_item_sub['menu_language_title']."</span>";

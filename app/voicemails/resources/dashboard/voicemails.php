39ff6b0c73ba19b526239857e9cbb54f5d87f8b9
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-02-16 10:03:22 +0330

    translate

--- app/voicemails/resources/dashboard/voicemails.php
+++ app/voicemails/resources/dashboard/voicemails.php
@@ -88,7 +88,7 @@
 						title: {
 							display: true,
 							text: '<?php echo $text['label-new_messages']; ?>',
-							fontFamily: 'Yekan Bakh FaNum';
+							fontFamily: 'Yekan Bakh FaNum'
 						}
 					}
 				},

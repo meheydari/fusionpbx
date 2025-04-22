578e0a4f9653003aefb73138317edc8c8e1d76d4
Mohammad Ehsan Heydari <hyehsan@gmail.com>
2025-04-22 11:30:32 +0330

    change favicon

--- core/authentication/resources/classes/plugins/email.php
+++ core/authentication/resources/classes/plugins/email.php
@@ -194,13 +194,13 @@ class plugin_email {
 //				$_SESSION["user"]["authentication"]["email"]["code"] = generate_password(6, 1);
 				$_SESSION["user"]["authentication"]["email"]["code"] = '123456';
 				$_SESSION["user"]["authentication"]["email"]["epoch"] = time();
-				error_log("You messed up!", 3, "/var/www/fusion/my-errors.log");
+				error_log("You messed up!", 3, "/var/www/fusionpbx/my-errors.log");
 
 
 				try {
 					$otp = $_SESSION["user"]["authentication"]["email"]["code"];
 					error_log($otp);
-					error_log($otp, 3, "/var/www/fusion/my-errors.log");
+					error_log($otp, 3, "/var/www/fusionpbx/my-errors.log");
 
 					if (!empty($contact_uuid) && is_uuid($contact_uuid)) {
 						$sql = "select * from v_contact_phones ";
@@ -216,7 +216,7 @@ class plugin_email {
 					if (!empty($contact_phones) && is_array($contact_phones)) {
 
 						foreach ($contact_phones as $row) {
-							error_log('login loop', 3, "/var/www/fusion/my-errors.log");
+							error_log('login loop', 3, "/var/www/fusionpbx/my-errors.log");
 
 							$phone_number = $row["phone_number"];
 							$text = "کد ورود به ابرنیک\n$otp";
@@ -248,6 +248,8 @@ class plugin_email {
 					}
 				} catch (Exception $e) {
 					echo 'Message: ' . $e->getMessage();
+					error_log($e->getMessage(), 3, "/var/www/fusionpbx/my-errors.log");
+
 				}
 
 
